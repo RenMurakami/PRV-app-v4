b9f4c960b4f2de956b4d6aee78617ce4991772c9
Ren Murakami <rmurakami@cedarville.edu>
2023-10-24 12:03:49 -0400

    Made 2 pages

---
+++ app/src/main/java/com/example/prv_app_v4/select_profile.kt
@@ -0,0 +1,14 @@
+package com.example.prv_app_v4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class select_profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_profile)
+    }
+
+
+}
+
